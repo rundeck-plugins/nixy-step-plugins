463e516c68f2f4422070c524d6609f376101eba4
Alex Honor <alexhonor@yahoo.com>
2016-11-02 12:14:26 -0700

    added build script

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -eu
+
+: ${BASEDIR:=.}
+: ${BUILDIR:=$BASEDIR/build}
+
+mkdir -p $BUILDIR/dist
+
+cd $BASEDIR
+BASENAME=$(basename $(pwd))
+
+PLUGINS=( $(for f in */plugin.yaml; do dirname $f; done) )
+
+package() {
+	local build_dir=$1 plugin=$2
+	mkdir -p $build_dir/$BASENAME-$plugin
+	cp -r $plugin/* $build_dir/$BASENAME-$plugin
+	(cd $build_dir &&
+	zip -r dist/${BASENAME}-${plugin}.zip $BASENAME-$plugin)
+}
+
+install() {
+	local build_dir=$1 plugin=$2
+	cp -v $BUILDIR/dist/${BASENAME}-${plugin}.zip $RDECK_BASE/libext
+}
+
+
+package_all() {
+	local plugins=($*)
+	for plugin in ${plugins[*]:-}
+	do
+		package $BUILDIR $plugin
+	done
+}
+
+install_all() {
+	local plugins=($*)
+	for plugin in ${plugins[*]:-}
+	do
+		install $BUILDIR $plugin
+	done	
+}
+
+# Package the plugins
+package_all ${PLUGINS[*]}
+
+# Copy the plugins to the libext directory
+if [[ -n "${RDECK_BASE:-}" ]]
+then
+	install_all ${PLUGINS[*]}
+fi
+
+exit $?
+# end.
