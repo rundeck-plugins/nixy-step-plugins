56b7305ffd830548621491aa5ba930b08146a17e
Alex Honor <alexhonor@yahoo.com>
2017-05-12 15:49:17 -0700

    fix bug in build script that named target files incorrectly

--- build.sh
+++ build.sh
@@ -1,55 +1,53 @@
 #!/usr/bin/env bash
 set -eu
 
-: ${BASEDIR:=.}
-: ${BUILDIR:=$BASEDIR/build}
+: "${BASEDIR:=.}"
+: "${BUILDIR:=$BASEDIR/build}"
 
-mkdir -p $BUILDIR/dist
+mkdir -p "$BUILDIR/dist"
 
-cd $BASEDIR
-BASENAME=$(basename $(pwd))
+cd "$BASEDIR"
+BASENAME=$(basename "$(pwd)")
 
-PLUGINS=( $(for f in */plugin.yaml; do dirname $f; done) )
+PLUGINS=( $(for f in */plugin.yaml; do dirname "$f"; done) )
 
 package() {
-	local build_dir=$1 plugin=$2
-	local version=$(awk '/version: / {print $2}' $plugin/plugin.yaml)
-	mkdir -p $build_dir/$BASENAME-$plugin
-	cp -r $plugin/* $build_dir/$BASENAME-$plugin
-	(cd $build_dir &&
-	zip -r dist/${BASENAME}-${plugin}-${version}.zip $BASENAME-$plugin)
+	local -r build_dir=$1 plugin=$2
+	mkdir -p "$build_dir/$BASENAME-$plugin"
+	cp -r "$plugin"/* "$build_dir/$BASENAME-$plugin"
+	(cd "$build_dir" &&
+	zip -r "dist/${BASENAME}-${plugin}.zip" "$BASENAME-$plugin")
 }
 
 install() {
-	local build_dir=$1 plugin=$2
-	local version=$(awk '/version: / {print $2}' $plugin/plugin.yaml)
-	cp -v $BUILDIR/dist/${BASENAME}-${plugin}-${version}.zip $RDECK_BASE/libext
+	local -r build_dir=$1 plugin=$2
+	cp -v "$BUILDIR/dist/${BASENAME}-${plugin}.zip" "$RDECK_BASE/libext"
 }
 
 
 package_all() {
-	local plugins=($*)
+	local -ra plugins=("$@")
 	for plugin in ${plugins[*]:-}
 	do
-		package $BUILDIR $plugin
+		package $BUILDIR "$plugin"
 	done
 }
 
 install_all() {
-	local plugins=($*)
+	local -ra plugins=("$@")
 	for plugin in ${plugins[*]:-}
 	do
-		install $BUILDIR $plugin
+		install $BUILDIR "$plugin"
 	done	
 }
 
 # Package the plugins
-package_all ${PLUGINS[*]}
+package_all "${PLUGINS[@]}"
 
 # Copy the plugins to the libext directory
 if [[ -n "${RDECK_BASE:-}" ]]
 then
-	install_all ${PLUGINS[*]}
+	install_all "${PLUGINS[@]}"
 fi
 
 exit $?
